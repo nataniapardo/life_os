95136facd47a94a64cdff50b620e995c8e5bcabf
nataniapardo <nataniapardo@gmail.com>
2026-04-13 15:42:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // =========================
 // SUPABASE SETUP
 // =========================
-const SUPABASE_URL = "https://bzwnjtofcduxllafdybw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_oFhZq2o2Ao5800xY2xzhFw_WOgTUHUl";
+const SUPABASE_URL = 'https://bzwnjtofcduxllafdybw.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_oFhZq2o2Ao5800xY2xzhFw_WOgTUHUl';
 
 // CHANGED NAME: supabaseClient → db
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
