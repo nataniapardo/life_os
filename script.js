e8c3a9a0efb911dc5b2e366b476bedea5ef30ef2
nataniapardo <nataniapardo@gmail.com>
2026-05-01 06:04:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// =========================
+// SUPABASE SETUP
+// =========================
+const SUPABASE_URL = 'https://bzwnjtofcduxllafdybw.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_oFhZq2o2Ao5800xY2xzhFw_WOgTUHUl';
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 /* ============================================================
    AUTH SYSTEM — Login, Sign Up, Session, Sign Out
    Uses localStorage to store hashed accounts securely.
