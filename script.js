7795b38149d7065ca128eda46b42909db1e83cfe
nataniapardo <nataniapardo@gmail.com>
2026-05-06 14:20:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // ==========================
 const SUPABASE_URL = 'https://bzwnjtofcduxllafdybw.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_oFhZq2o2Ao5800xY2xzhFw_WOgTUHUl';
-
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 document.addEventListener('DOMContentLoaded', () => {
